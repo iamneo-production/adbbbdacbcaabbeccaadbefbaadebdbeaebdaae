543879c0b49ea74ecd877068faeb3da98e9511de
Iamneo <techadmin@iamneo.ai>
2023-08-07 18:49:30 +0000

    Backup Commit

--- atmapp/jspservlet-app-ATM/src/main/java/model/AccountDetails.java
+++ atmapp/jspservlet-app-ATM/src/main/java/model/AccountDetails.java
@@ -2,5 +2,44 @@ public class AccountDetails {
     private String accountType;
     private double withdrawOrDepositAmount;
     private double availableBalance;
-    
+    private int pin;
+
+    public AccountDetails(String accType, double wod, double availBal, int pin) {
+        this.accountType = accType;
+        this.withdrawOrDepositAmount = wod;
+        this.availableBalance = availBal;
+        this.pin = pin;
+    }
+
+    public getAccountType() {
+        return this.accountType;
+    }
+
+    public setAccountType(String accType) {
+        this.accountType = accType;
+    }
+
+    public getWOD() {
+        return this.withdrawOrDepositAmount;
+    }
+
+    public setWOD(double value) {
+        this.withdrawOrDepositAmount = value;
+    }
+
+    public getAvailBal() {
+        return this.availableBalance;
+    }
+
+    public setAvailBal(double bal) {
+        this.availableBalance = bal;
+    }
+
+    public getPin() {
+        return this.pin;
+    }
+
+    public setPin(int pin) {
+        this.pin = pin;
+    }
 }
